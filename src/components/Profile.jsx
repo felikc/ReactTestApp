784092567139ba2ff899754058adcf87e85547c2
Felikc <felikc@gmail.com>
2019-12-14 14:43:40 +0200

    add components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://www.kmeta.bg/uploads/municipality/header_image/16/burgas_.jpg" alt="" />
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+          <div>
+                New Post
+          </div>
+            <div>Post1</div>
+            <div>Post2</div>
+        </div>
+        Main content
+      </div>
+}
+
+export default Profile;
